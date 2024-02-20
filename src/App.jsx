f80a593a6d3bf8d10b4be5a9d76257bb771448a5
HyeonSeok <gustjr2332@naver.com>
2024-02-20 16:21:56 +0900

    feat: 폰트, 임시 이미지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import './App.css';
 import StartPage from './component/StartPage';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Question from './component/Question';
-import {optionList, questionList} from './questionList';
+import {optionList, questionList, resultList} from './questionList';
 import Result from './component/Result';
 import Loding from './component/Loding';
+
 function App() {
   const [answers, setAnswers] = useState([])
   const [page,setPage] = useState(0);
@@ -30,17 +31,19 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<StartPage/>}/>
-          <Route path="/Loding" element={<Loding/>}/>
+          <Route path="/result" element={<Result/>}/>
+          <Route path="/loding" element={<Loding/>}/>
           <Route path="/test" 
           element={
           page<questionList.length?
           <Question 
             question={questionList[page]}
             options={optionList[page].options}
+            imgPath={optionList[page].imgPath}
             addAnswer={addAnswer}
             deleteAnswer={deleteAnswer}
           />
-          :<Result answers={answers}/>
+          :<Loding answers={answers} resultList={resultList}/>
         }/>
         
         </Routes>
