a3193a405d77ccac0e9bd3f2933b53f2f2ccfe77
HyeonSeok <gustjr2332@naver.com>
2024-02-20 16:22:16 +0900

    feat: 결과 출력 기능 추가

---
+++ src/component/Result.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useNavigate  } from "react-router-dom";
+import styled, { StyleSheetManager } from "styled-components";
+function Result(){
+    const location = useLocation();
+    const answers = location.state.answers;
+    const resultList = location.state.resultList;
+
+    const [resultType,setResultType] = useState('');
+    const [resultImg,setResultImg] = useState('');
+    const [result,setResult] = useState('');
+    const [parts,setParts] = useState([]);
+
+    const highlightedWords = ['간장', '고추장', '얼큰한', '구수한', '고기파', '생선파'];
+    const answerCounts = {
+        E:0,
+        I:0,
+        S:0,
+        N:0,
+        T:0,
+        F:0,
+    }
+
+    useEffect(()=>{ 
+        const result = [
+            answerCounts['E'] > answerCounts['I'] ? 'E' : 'I',
+            answerCounts['S'] > answerCounts['N'] ? 'S' : 'N',
+            answerCounts['T'] > answerCounts['F'] ? 'T' : 'F',
+        ].join('')
+        setResult(result);
+    },[])
+    
+    useEffect(() => {
+        if (resultList[result]) {
+            setResultType(resultList[result][0]);
+            setParts(resultList[result][0].split(', '));
+            setResultImg(resultList[result][1]);
+            console.log(resultList[result][0].split(', '));
+        }
+
+    }, [result]);
+
+
+    return(
+        <>
+         <StyleSheetManager shouldForwardProp={(prop) => prop !== 'ishighlighted'}>
+            <Container>
+            <ResultImage src={resultImg}/>
+            <div>
+                {result}
+            </div>
+
+        {parts.map((phrase, index) => (
+            <div key={index}>
+                {phrase.split(' ').map((word, idx) => (
+                <StyledSpan key={idx} ishighlighted={ highlightedWords.includes(word) ? 'true' : undefined}>
+                    {word}{' '}
+                </StyledSpan>
+                ))}
+            <br />
+            </div>
+            ))}
+            </Container>
+        </StyleSheetManager>
+        </>
+    )
+}
+
+
+export default Result;
+
+const Container = styled.div`
+font-family: "monggle";
+p{
+    margin:0px
+}
+
+`
+
+const ResultImage = styled.img`
+    width: 30%;
+    height: 30%;
+    min-width:150px;
+    min-height:150px;
+    max-width:250px;
+    max-height:250px;
+`
+
+const StyledSpan = styled.span`
+    color: ${({ ishighlighted }) => ishighlighted ? '#F44336' : 'black'};
+    font-size: ${({ ishighlighted }) => ishighlighted ? '30px' : '20px'};
+`
---
+++ src/questionList.js
@@ -0,0 +1,100 @@
+export const optionList = [
+    {
+        options: ['일단사진', '다섯글자이상'],
+        imgPath: ['/image/임시사진.PNG' , '/image/임시사진2.PNG']
+    },
+    {
+        options: ['그렇다3.', '아니다4.'],
+        imgPath: ['/image/임시사진2.PNG' , '/image/임시사진.PNG']
+    },
+    {
+        options: ['그렇다5.', '아니다4.'],
+        imgPath: ['/image/임시사진2.PNG' , '/image/임시사진.PNG']
+    },
+    {
+        options: ['그렇다7.', '아니다4.'],
+        imgPath: ['/image/임시사진2.PNG' , '/image/임시사진.PNG']
+    },
+    {
+        options: ['그렇다9.', '아니다4.'],
+        imgPath: ['/image/임시사진2.PNG' , '/image/임시사진2.PNG']
+    },
+    {
+        options: ['그렇다11.', '아니다4.'],
+        imgPath: ['/image/임시사진.PNG' , '/image/임시사진.PNG']
+    },
+    {
+        options: ['그렇다13.', '아니다4.'],
+        imgPath: ['/image/임시사진2.PNG' , '/image/임시사진.PNG']
+    },
+    {
+        options: ['그렇다15.', '아니다4.'],
+        imgPath: ['/image/임시사진.PNG' , '/image/임시사진2.PNG']
+    },
+    {
+        options: ['그렇다17.', '아니다4.'],
+        imgPath: ['/image/임시사진2.PNG' , '/image/임시사진.PNG']
+    },
+];
+
+
+
+export const questionList = [
+    {
+        question: "1. 질문이 너무 길어져도 잘 출력되는 걸 볼 수 있다.",
+        A: "E",
+        B: "I"
+    },
+    {
+        question: "2. 질문입니다.",
+        A: "S",
+        B: "N"
+    },
+    {
+        question: "3. 질문입니다.",
+        A: "T",
+        B: "F"
+    },
+    {
+        question: "4. 질문입니다.",
+        A: "E",
+        B: "I"
+    },
+    {
+        question: "5. 질문입니다.",
+        A: "T",
+        B: "F"
+    },
+    {
+        question: "6. 질문입니다.",
+        A: "S",
+        B: "N"
+    },
+    {
+        question: "7. 질문입니다.",
+        A: "E",
+        B: "I"
+    },
+    {
+        question: "8. 질문입니다.",
+        A: "T",
+        B: "F"
+    },
+    {
+        question: "9. 질문입니다.",
+        A: "S",
+        B: "N"
+    },
+]
+
+
+export const resultList = {
+    ESF:["얼큰한 국물, 고추장 베이스의 반찬, 고기파 밥상이 완성됐어요!!", '/image/임시사진2.PNG'],
+    EST:["얼큰한 국물, 고추장 베이스의 반찬, 생선파 밥상이 완성됐어요!!", '/image/임시사진2.PNG'],
+    ENF:["구수한 국물, 고추장 베이스의 반찬, 고기파 밥상이 완성됐어요!!", '/image/임시사진2.PNG'],
+    ENT:["구수한 국물, 고추장 베이스의 반찬, 생선파 밥상이 완성됐어요!!", '/image/임시사진2.PNG'],
+    ISF:["얼큰한 국물, 간장 베이스의 반찬, 고기파 밥상이 완성됐어요!!", '/image/임시사진.PNG'],
+    IST:["얼큰한 국물, 간장 베이스의 반찬, 생선파 밥상이 완성됐어요!!",'/image/임시사진.PNG'],
+    INF:["구수한 국물, 간장 베이스의 반찬, 고기파 밥상이 완성됐어요!!",'/image/임시사진.PNG'],
+    INT:["구수한 국물, 간장 베이스의 반찬, 생선파 밥상이 완성됐어요!!",'/image/임시사진.PNG'],
+}
